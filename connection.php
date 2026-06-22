64b6414e12bed5ca9ca56299b843da83f54001bd
Victor Silva <victorsilva.lion@gmail.com>
2026-06-22 00:05:11 -0300

    Código refatorado: Implementação de tratamento de erros e exceção

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
 <?php 
 
-$connect = mysqli_connect(
-    'db', 
-    'root', 
-    $_ENV['MYSQL_ROOT_PASSWORD'], 
-    'estoque'
-);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    $connect = mysqli_connect(
+        'db', 
+        'root', 
+        $_ENV['MYSQL_ROOT_PASSWORD'], 
+        'estoque'
+    );
+} catch (mysqli_sql_exception $error) {
+    error_log($error->getMessage());
+    die('Erro na conexão com o banco de dados.');
+}
